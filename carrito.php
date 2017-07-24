ec566d1ab01c353adff525f4597af9c7756361e0
Dayaliz <adrimofk01_@hotmail.com>
2017-07-24 04:14:22 -0500

    version1 aniadiendo

---
+++ carrito.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<body bgcolor="#D2EBF" style="font-family: Tahoma;" >
+    <?php
+     echo "<h2>Usuario:". $_SESSION["nombre"]."".$$_SESSION["apellido"];
+     if(count($_SESSION["carrito"])==0)
+     {
+         echo "<h3>Carrtio vacio</h3>";
+         
+     }
+     else
+     {
+         echo "<table widht='400' border='1'>";
+         foreach ($_SESSION["carrito"]as $articulo => $cantidad)
+             echo "<tr>";
+             echo "<td>$articulo</td>";
+             echo "<td>$cantidad</td>";
+             echo "</tr>";
+     }
+     echo "</table>";
+        
+     ?>
+    <a href="agregararticulo.php">Agregar Articulo</a>
+    <a href="cerraarsesion.php">Cerrar Sesion</a>
+</body>
